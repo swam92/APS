6f749b7abff0f9784e36298e82e2b24975648545
Sam Wizon <swam92@172-17-14-141.DYNAPOOL.NYU.EDU>
2015-02-11 14:51:13 -0500

    not working

--- hw1/Calendar1.java
+++ hw1/Calendar1.java
@@ -13,10 +13,10 @@ class Calendar1{
 		int interval = 0;
 		
         while (in.hasNext()) {
-			boolean flag = false;
+			boolean overlapFlag = false;
 			int nonRepeating = in.nextInt();
 			int repeating = in.nextInt();
-        	int[] calendar = new int[1000000];
+        	int[] calendar = new int[1000001];
         	
         	if(nonRepeating==0 && repeating==0){
         		break;
@@ -26,23 +26,29 @@ class Calendar1{
 
             	int start=in.nextInt();
             	int finish = in.nextInt();
+                int dur= finish-start;
             	if(start==0 && finish==0){
                     return;
             	}
-            	for(int k= start; k < finish; k++){
-            		if(calendar[k]==1){
-            			flag = true;
+            	for(int k= start; k <= finish; k++){
+            		if(calendar[k]==1 && k!=finish && k!=start){
+            			overlapFlag = true;
             			break;
             		}
+                    else if(calendar[k]==1 && dur==1 && calendar[start-1]==1 && calendar[finish+1]==1){
+                        overlapFlag=true;
+                        break;
+                    }
                     else{
-            		calendar[k] = 1;
+                      //System.out.println(k + " first");
+            		  calendar[k] = 1;
                     }
             	}
-            	if(flag==true){
+            	if(overlapFlag==true){
             		break;
             	}
             }
-            if(flag==false){
+            if(overlapFlag==false){
             	for(int j=0; j<repeating;j++){
             		int start=in.nextInt();
             		int finish = in.nextInt();
@@ -53,38 +59,43 @@ class Calendar1{
                     if(finish==0&&interval==0){
                         return;
                     }
-            		int p;
             		int duration = finish - start;
-
-                    //find each offset
-                    for(int k=start; k<=finish; k+=interval){
-            			if(start>1000000){
-            				break;
-            			}
-
-                        //index from offset to finish	
-            			for(p=start;p<=finish;p++){
-                            if(p < 1000000){
-            					if(calendar[p]==1 && p!=finish){
-            						flag=true;
-            						break;
-            					}
-            					calendar[p]=1;
+                    while(start<1000000 && overlapFlag==false){
+                        for(int k=start; k<=finish; k++){
+                            if(k < 1000000){
+                                if(calendar[k]==1 && k!=finish && k!=start){
+                                    overlapFlag=true;
+                                    break;
+                                }
+                                else if(calendar[k]==1 && duration==1 && calendar[start-1]==1 && calendar[finish+1]==1){
+                                    overlapFlag=true;
+                                    break;
+                                }
+                                //System.out.println(k + " second");
+                                 calendar[k]=1;
                             }
                             else{
+                                start = 1000002;
                                 break;
                             }
-            			}
-                         //if theres an overlap
-            		     if(flag==true){
-            				break;
-            			}
-            		}
-            	}
 
+            		    }//end for loop
+                        start+=interval;
+                        finish+=interval;
+
+                        if(overlapFlag==true){
+                            break;
+                        }	    
+                    }//end while loop
+                
+                if(overlapFlag==true){
+                    break;
+                }
+
+                }//end repeating loop
         	}
 
-        	if(flag==false){
+        	if(overlapFlag==false){
         		System.out.println("NO CONFLICT");
         	}
         	else{

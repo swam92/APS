230424f4cd8e95db1c13c60808adece77415010a
Sam Wizon <swam92@172-16-186-217.DYNAPOOL.NYU.EDU>
2015-02-16 11:00:29 -0500

    finished religion problem using union find on array

--- hw1/Religion.java
+++ hw1/Religion.java
@@ -3,47 +3,77 @@ import java.io.*;
 
 class Religion{
 
+	static int [] array;
+
+	public static int find(int x){
+		if(array[x] != x){
+			array[x] = find(array[x]);
+		}
+		
+		return array[x];
+	}
+
+	public static void union(int x, int y){
+		int xRoot = find(x);
+		int yRoot = find(y);
+		array[yRoot] = xRoot;
+	}
+
 	public static void main(String[] args){
 		
 		Scanner in = new Scanner(System.in);
 		int caseCount = 0;
-		
+		int count;
+
 		while(in.hasNext()){
-			caseCount++;
+			count = 0;
 			int numStudents = in.nextInt();
 			int m = in.nextInt();
 
 			if(numStudents==0 && m ==0){
-				break;
+				return;
 			}
-			int[] peopleArray = new int[numStudents+1];
+			caseCount++;
+
+			array = new int[numStudents+1];
+			for(int j=1;j<array.length;j++){
+				array[j] = j;
+			}
 
 			for(int i=0; i < m; i++){
 				int studentOne = in.nextInt();
 				int studentTwo = in.nextInt();
-				
-				if(peopleArray[studentTwo] == 0){
-					peopleArray[studentTwo] = studentOne;
-				}
-				else{
-					int index = peopleArray[studentTwo];
-					int previous = 0;
-					while(index != 0){
-						previous = index;
-						index = peopleArray[index];
-					}
-					peopleArray[studentTwo] = previous;
+				if(studentOne==0&&studentTwo==0){
+					return;
 				}
 
+				if(array[studentTwo] == studentTwo){
+					int studentOneParent = find(studentOne);
+					union(studentOne,studentTwo);
+				}
+				else if(array[studentTwo] != studentTwo){
+					int studentTwoParent = find(studentTwo);
+					int studentOneParent = find(studentOne);
+					union(studentOneParent,studentTwoParent);
+				}
 			}//end for
-		int count =0;
-		for(int k=1; k < peopleArray.length;k++){
-			if(peopleArray[k]==0){
-				count++;
-			}
-		}
-		System.out.println("Case " + caseCount + ": " + count);
 
+			for(int k=1; k< array.length;k++){
+				if(array[k]!= k){
+					count++;
+				}
+			}
+
+			/*for(int y=1;y<array.length;y++){
+				System.out.println(array[y]);
+			}*/
+
+		if(count==1){
+			System.out.println("Case " + caseCount + ": " + count);
+		}
+		else{
+		System.out.println("Case " + caseCount + ": " + (numStudents- count) );
+		}
 
 		}//end while
 	

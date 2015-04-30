12ad38c039883d45fa7de926d1775ebea0b9cede
swam92 <svw223@nyu.edu>
2015-04-30 11:29:19 -0400

    adding hw10

---
+++ hw10/C.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class C{
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+
+	}
+
+
+}
---
+++ hw10/b.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class B{
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int cases = Integer.parseInt(in.readLine());
+
+		for(int i=0; i < cases; i++){
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			double ax = Double.parseDouble(st.nextToken());
+			double ay = Double.parseDouble(st.nextToken());
+			double bx = Double.parseDouble(st.nextToken());
+			double by = Double.parseDouble(st.nextToken());
+
+			
+
+		}
+
+
+	}
+
+
+}
